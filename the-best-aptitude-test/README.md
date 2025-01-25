662023be9765f63154fafa02ec79ede23dd1a3e9
maku77 <makkuma77@gmail.com>
2025-01-26 03:20:12 +0900

    The Best Aptitude Test

---
+++ the-best-aptitude-test/README.md
@@ -0,0 +1,18 @@
+# The Best Aptitude Test
+
+https://www.hackerrank.com/challenges/the-best-aptitude-test/problem?isFullScreen=true
+
+## Run the code
+
+```bash
+uv run main.py < input.txt
+```
+
+## Solution / Ideas
+
+与えられた Grade Point Average (GPA) と各科目のスコアの相関係数を求めて、最も相関係数が高い（1.0 に近い）科目を見つければよい。
+ピアソンの積率相関係数は以下の式で求められる。
+
+$$
+r = \frac{\sum(x_i - \bar{x})(y_i - \bar{y})}{\sqrt{ \sum(x_i - \bar{x})^2 \cdot \sum(y_i - \bar{y})^2}}
+$$
---
+++ the-best-aptitude-test/main.py
@@ -0,0 +1,26 @@
+def read_nums() -> list[float]:
+    return [float(x) for x in input().split()]
+
+
+def cc(x: list[float], y: list[float]) -> float:
+    """Compute the Pearson correlation coefficient between two lists of numbers."""
+    n = len(x)
+    sum_x, sum_y = sum(x), sum(y)
+    sum_x_sq, sum_y_sq = sum([i**2 for i in x]), sum([i**2 for i in y])
+    sum_xy = sum([i * j for i, j in zip(x, y)])
+    numerator = n * sum_xy - sum_x * sum_y
+    denominator = ((n * sum_x_sq - sum_x**2) * (n * sum_y_sq - sum_y**2)) ** 0.5
+    if denominator == 0:
+        return 0
+    return numerator / denominator
+
+
+def solve() -> None:
+    input()  # Skip N: the number of students
+    averages = read_nums()
+    ccs = [cc(averages, read_nums()) for _ in range(5)]
+    print(ccs.index(max(ccs)) + 1)
+
+
+for _ in range(int(input())):
+    solve()
